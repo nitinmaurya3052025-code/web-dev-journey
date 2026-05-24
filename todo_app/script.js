7bb197d06920373a11de5806f16c9fece0c3a0c5
Nitin <nitinmaurya3052025@gmail.com>
2026-05-25 01:07:38 +0530

    Updated todo_app version with local storage additon

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,6 +1,44 @@
 let taskInput = document.getElementById("taskInput");
 let addBtn = document.getElementById("addBtn");
 let taskList = document.getElementById("taskList");
+let tasks = [];
+let savedTasks = localStorage.getItem("tasks");
+if (savedTasks) {
+  tasks = JSON.parse(savedTasks);
+}
+function renderTask(task) {
+  let li = document.createElement("li");
+  li.innerText = task.text;
+  li.classList.add(task.priority);
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  if (task.completed) {
+    li.classList.add("completed");
+  }
+  checkbox.addEventListener("change", function () {
+    li.classList.toggle("completed");
+    task.completed = checkbox.checked;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+  let deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+    //IMPORTANT FOR FILTERING AND ALSO WITH WHOLE LOOP TRAVERSAL
+    tasks = tasks.filter(function (t) {
+      return t !== task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+  li.prepend(checkbox);
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+}
+//IMPORTANT LOOP TRAVERSAL
+tasks.forEach(function (task) {
+  renderTask(task);
+});
 
 addBtn.addEventListener("click", function () {
   let taskText = taskInput.value;
@@ -8,22 +46,13 @@ addBtn.addEventListener("click", function () {
     return;
   }
   let priority = document.getElementById("priority").value;
-  let li = document.createElement("li");
-  li.innerText = taskText;
-  li.classList.add(priority);
-  let checkbox = document.createElement("input");
-
-  checkbox.type = "checkbox";
-  checkbox.addEventListener("change", function () {
-    li.classList.toggle("completed"); //added class for checkbox
-  });
-  let deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "Delete";
-  deleteBtn.addEventListener("click", function () {
-    li.remove();
-  });
-  li.prepend(checkbox);
-  taskList.appendChild(li);
-  li.appendChild(deleteBtn);
+  let task = {
+    text: taskText,
+    priority: priority,
+    completed: false,
+  };
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTask(task);
   taskInput.value = "";
 });
