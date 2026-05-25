64a7ba9b0f76e8580eb663d3e1894b03d774e4a1
Nitin <nitinmaurya3052025@gmail.com>
2026-05-25 22:27:51 +0530

    Learn React fundamentals including state, props, lists, and controlled inputs

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,13 +1,37 @@
-function Welcome(props) {
-  return <h1>Hello {props.name}</h1>;
-}
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [login, setLogin] = useState(false);
+  const tasks = ["Study React", "Build Project", "Push to GitHub"];
+  const [text, setText] = useState("");
   return (
     <div>
-      <Welcome name="Nitin" />
-      <Welcome name="Rahul" />
-      <Welcome name="Piyush" />
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <br></br>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <h1>{login ? "Welcome User" : "Please Login"}</h1>
+      <button onClick={() => setLogin(!login)}>login</button>
+      <h1>My Task</h1>
+      <ul>
+        {tasks.map(function (task) {
+          return <li>{task}</li>;
+        })}
+      </ul>
+      <h1>My Task</h1>
+      <ul>
+        {tasks.map(function (task, index) {
+          return <li key={index}>{task}</li>;
+        })}
+      </ul>
+      <input
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+
+      <h1>{text}</h1>
     </div>
   );
 }
