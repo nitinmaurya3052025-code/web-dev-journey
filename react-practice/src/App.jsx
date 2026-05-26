24c0849f645cb556c83cf0ec1b06bf03da2be73c
Nitin <nitinmaurya3052025@gmail.com>
2026-05-26 15:29:36 +0530

    My First API Fetching done

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
